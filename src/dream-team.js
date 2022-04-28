41d8a64e36226dd976144e853f6ec9bf3bef650b
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-04-28 19:44:14 +0300

    feat: task createDreamTeam complete

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members) {
+  
+  let arr = members
+  if (!Array.isArray(arr)){
+    return false
+  }
 
+  arr.sort()
+  let result = ''
+  for (let name in arr) {
+    typeof arr[name] === 'string'?
+    arr[name]=arr[name].trim().toUpperCase():
+    false;
+  }
+  arr.sort()
+  console.log(arr)
+  for (let name of arr) {
+    if (typeof name === 'string'){
+      if (name[0]!==undefined){
+        result+=name[0]
+      }
+      
+    }
+    
+    
+    
+  }
+  
+  return result
+}
+createDreamTeam([
+  ['David Abram'],
+  ['Robin Attfield'],
+  'Thomas Berry',
+  ['Paul R.Ehrlich'],
+  'donna Haraway',
+  ' BrIaN_gOodWiN  ',
+  {
+    0: 'Serenella Iovino'
+  },
+  'Erazim Kohak',
+  '  val_plumwood',
+])
 module.exports = {
   createDreamTeam
 };
